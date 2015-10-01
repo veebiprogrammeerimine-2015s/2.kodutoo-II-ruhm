6bd536613ceb6614130b6c3eacdf354a753f36ea
Kert Kulp <kertkul@greeny.cs.tlu.ee>
2015-10-02 00:49:13 +0300

    Tried to add functionality to make database entries

--- page/login.php
+++ page/login.php
@@ -13,7 +13,9 @@
 
 <?php
 
-	//echo $_POST["email"];
+	require_once("../../config.php");
+	$database = "if15_kertkulp";
+	$mysqli = new mysqli($servername, $username, $password, $database);
 	
 	//defineerime muutujad
 	$email_error = "";
@@ -22,6 +24,13 @@
 	$lastname_error = "";
 	$create_password_error = "";
 	$create_email_error = "";
+	
+	$email = "";
+	$password = "";
+	$createemail = "";
+	$createpassword = "";
+	$firstname = "";
+	$firstname = "";
 	
 	//kontrollin kas keegi vajutas nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -34,10 +43,14 @@
 			//Kas epost on tühi
 			if( empty($_POST["email"]) ) {
 				$email_error = "E-post on kohustuslik!";
+			}else{
+				$email = cleanInput($_POST["email"]);
 			}
 			//Kas parool on tühi
 			if( empty($_POST["password"]) ) {
 				$password_error = "Parool on kohustuslik!";
+			}else{
+				$password = cleanInput($_POST["password"]);
 			}
 		
 		} elseif(isset($_POST["create"])){
@@ -47,31 +60,76 @@
 			//Kas epost on tühi
 			if( empty($_POST["createemail"]) ) {
 				$create_email_error = "E-post on kohustuslik!";
+			}else{
+				$createemail = cleanInput($_POST["createemail"]);
 			}
 			//Kas parool on tühi
-			if( empty($_POST["createpassword"]) ) {
-				$create_password_error = "Parool on kohustuslik!";
+			if ( empty($_POST["createpassword"]) ) {
+				$create_password_error = "See väli on kohustuslik";
+			} else {
+				if(strlen($_POST["createpassword"]) < 8) {
+					$createpassworderror = "Peab olema vähemalt 8 tähemärki pikk!";
+				}else{
+					$createpassword = cleanInput($_POST["createpassword"]);
+				}
 			}
+			
 			
 			if( empty($_POST["firstname"]) ) {
 				$firstname_error = "Eesnimi on kohustuslik!";
+			}else{
+				$firstname = cleanInput($_POST["firstname"]);
 			}
 			
 			if( empty($_POST["lastname"]) ) {
 				$lastname_error = "Perekonnanimi on kohustuslik!";
+			}else{
+				$firstname = cleanInput($_POST["firstname"]);
 			}
+			
+			if(	$create_email_error == "" && $create_password_error == "" && $firstname_error == "" && $lastname_error == ""){
+				echo "Võib kasutajat luua! Kasutajanimi on ".$createemail." ja parool on ".$createpassword;
+			
+				$password_hash = hash("sha512", $createpassword);
+				echo "<br>";
+				echo $password_hash;
+				
+				$stmt = $mysqli->prepare("INSERT INTO kasutajad (email, password) VALUE (?, ?)");
+				
+				//echo $mysqli->error;
+				//echo $stmt->error;
+				
+				
+				//asendame küsimärgid muutujate väärtustega
+				//ss - s tähendab string iga muutjua kohta
+				$stmt->bind_param("ss", $create_email, $password_hash);
+				$stmt->execute();
+				$stmt->close();
+			}
+		}
 		
 		}
+	
+	
+	
+	function cleanInput($data){
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
 	}
+	
+	
+	$mysqli->close();
+
 ?>
 
 
-
-
-
-
-
-		
+<html>
+	<head>
+		<title>Login</title>
+	</head>
+	<body>		
 		<h2>Login</h2>
 		<form action="login.php" method="post"> 
 			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?> <br><br>
@@ -92,9 +150,7 @@
 </html>
 
 
-
 <?php
-	//Kopeerime footer.php sisu
+	//Kopeerime header.php sisu
 	require_once("../footer.php")
-
 ?>
