ed6521dbe5f295311ca9a9c2068e963e51153578
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2015-10-01 16:09:35 +0300

    creating shablon

--- page/login.php
+++ page/login.php
@@ -12,35 +12,44 @@
 	$mysqli = new mysqli($servername, $username, $password, $database);
 
 	
-	// errori muutujad peavad igal juhul olemas olema 
-	$email_error = $password_error = $regname_error = $reglog_error = $lastname_error = $regpassword_error = $regemail_error = "";
+	// ВСЕ ОШИБКИ ИДУТ СЮДА, И ЕЩЕ В 2 ОПРЕДЕЛЕННЫХ МЕСТА ВНИЗУ.
+	$email_error = $password_error = "";
 	
 	$email = $regemail = "";
 	$password = $regpassword = "";
 	
-	// kontrollime et keegi vajutas input nuppu
+	// ПРОВЕРЯЕМ НАЖАЛ ЛИ КТО КНОПКУ СЛ
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		if(isset($_POST["login"])){
 		
 		//echo "ktonibuty nazhimal knopku";
-		//CREATE LOG ===========================================================================
-		//proverka pochty pustoy ili net
+		//======================================================================================
+		//===================CREATE LOG ========================================================
+		//======================================================================================
+		//======================================================================================
+		//ПОЧТА ПУСТАЯ ИЛИ НЕТ.===============================================================
+		//======================================================================================
 		if (empty($_POST["email"])) {
 			$email_error = "e-mail is required";
 		}else{
 			 $email = test_input($_POST["email"]);
-			// check if e-mail address is well-formed
+			//======================================================================================
+			// ПРОВЕРЯЕМ ЕСЛИ ПОЧТА В ПРАВЕЛЬНОМ ФОРМАТЕ.===========================================
+			//======================================================================================
 			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 			$emailErr = "Invalid email format"; 
 		}
 		}
-		
-		//proverka porolya, pustoi ili net
+		//======================================================================================
+		//ПРОВЕРКА ПАРОЛЯ, СТРОКА ПУСТА ИЛИ НЕТ.==============================================
+		//======================================================================================
 		if ( empty($_POST["password"])) {
 			$password_error = "password is required";
 		}else{
 				$password = test_input($_POST["password"]);
-				// check if pass only contains letters and whitespace
+				//======================================================================================
+				// ПРОВЕРКА ПАРОЛЯ, СОДЕРЖИТ ЛИ ЛИШНИЕ ЗНАКИ
+				//======================================================================================
 				if (!preg_match("/^[a-zA-Z ]*$/",$password)) {
 				$password = "Only letters and white space allowed"; 
 		}
@@ -50,59 +59,19 @@
 		
 			 if(isset($_POST["create"])){
 		
-			//CREATE NUPP ======================================================================
-			
-				// kas NIMI on tühi
-				if( empty($_POST["name"]) ) {
-				// registre name on tühi
-				$name_error = "Name is required";
-			}
-				if( empty($_POST["lastname"]) ) {
-				// lastname on tühi
-				$lastname_error = "perekonnanimi is required";
-			}
-				if( empty($_POST["login"]) ) {
-				// login on tühi
-				$reglog_error = "login is required";
-			}
-				if( empty($_POST["regpassword"]) ) {
-				// reg pass on tühi
-				$regpassword_error = "password is required";
-			}
-				if( empty($_POST["regname"]) ) {
-				// reg pass on tühi
-				$regname_error = "name is required";
-			}
-				if( empty($_POST["regemail"]) ) {
-				// reg pass on tühi
+			//======================================================================================
+			//===================СОЗДАНИЕ КНОПКИ "РЕГИСТРАЦИИ" CREATE===============================
+			//======================================================================================
+			if( empty($_POST["regemail"]) ) {
+				// ОШИБКА ПУСТОГО ПОЛЯ
 				$regemail_error = "email is required";
 			}
-				
-				$password_hash = hash("sha512", $create_password);
-				echo "<br>";
-				echo $password_hash;	
-				
-				$stmt = $mysqli->prepare("INSERT INTO usesdf_sample (email, password) VALUE (?, ?)");
-				
-				//echo $mysqli->error;
-				//echo $stmt->error;
-				//asendame ? märgid muutujate väärtuste
-				// ss - s tähendab string iga muutuja kohta
-				$stmt->bind_param("ss", $create_email, $password_hash);
-				$stmt->execute();
-				$stmt->close();
-			}	
-		
-		
-		
-	function test_input($data) {
-      $data = trim($data);
-      $data = stripslashes($data);
-      $data = htmlspecialchars($data);
-      return $data;
-    }
-				// paneme ühenduse kinni
-				$mysqli->close();
+			if( empty($_POST["regpassword"]) ) {
+				// ОШИБКА ПУСТОГО ПОЛЯ
+				$regpassword_error = "password is required";
+			}			
+			
+	}		
 ?>
 
 
@@ -123,11 +92,8 @@
 	
 		<h2>Create user</h2>
 		<form action="login.php" method="post">
-			<input name="regname" type="text" placeholder="eesnimi" > <?php echo $regname_error; ?><br><br>
-			<input name="lastname" type="text" placeholder="perekonnanim" > <?php echo $lastname_error; ?><br><br>
-			<input name="reglog" type="text" placeholder="login" > <?php echo $reglog_error; ?><br><br>
-			<input name="regpassword" type="password" placeholder="passi" > <?php echo $regpassword_error; ?><br><br>
 			<input name="regemail" type="email" placeholder="email" > <?php echo $regemail_error; ?><br><br>
+			<input name="regpassword" type="password" placeholder="passi" > <?php echo $regpassword_error; ?><br><br>
 			IF YOU WANT TO COMMENT THIS SITE, SO TRY IT <br><br>
 			<textarea name="comment" rows="5" cols="40"></textarea><br><br>
 			kas teil meeldib?<input type="radio" name="gender" value="female">Jah
